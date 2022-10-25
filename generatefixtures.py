e3003df87c53eaaefbd4a9c1891fe353222a257c
Shreeraj Pawar <52608782+zuther77@users.noreply.github.com>
2022-10-25 08:31:58 -0400

    Update generatefixtures.py

--- generatefixtures.py
+++ generatefixtures.py
@@ -2,10 +2,10 @@
 
 import csv
 import hashlib
-import os.path as path
+import os
 import random
 
-DIR = path.abspath(path.dirname(__file__))
+DIR = os.path.abspath(os.path.dirname(__file__))
 FILES = {
     'clothing.csv': ('Blouses', 'Shirts', 'Tanks', 'Cardigans', 'Pants', 'Capris', '"Gingham" Shorts',),
     'accessories.csv': ('Watches', 'Wallets', 'Purses', 'Satchels',),
@@ -17,17 +17,22 @@ def write_file(writer, length, categories):
     writer.writerow(['email_hash', 'category'])
     for i in range(0, length):
         writer.writerow([
-            hashlib.sha256('tech+test{}@pmg.com'.format(i).encode('utf-8')).hexdigest(),
+            hashlib.sha256(
+                'tech+test{}@pmg.com'.format(i).encode('utf-8')).hexdigest(),
             random.choice(categories),
         ])
 
 
 def main():
+    if not os.path.exists('./fixtures'):
+        os.makedirs('fixtures')
+
     for fn, categories in FILES.items():
-        with open(path.join(DIR, 'fixtures', fn), 'w', encoding='utf-8') as fh:
+        with open(os.path.join(DIR, 'fixtures', fn), 'w+', encoding='utf-8') as fh:
             write_file(
-                csv.writer(fh, doublequote=False, escapechar='\\', quoting=csv.QUOTE_ALL),
-                random.randint(100, 1000),
+                csv.writer(fh, doublequote=False, escapechar='\\',
+                           quoting=csv.QUOTE_ALL),
+                random.randint(7, 7),
                 categories
             )
 
