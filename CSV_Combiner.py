e1fd252df18f2ebcdcde5b09ceb10d5c1495cabc
Shreeraj Pawar <52608782+zuther77@users.noreply.github.com>
2022-10-25 06:17:23 -0400

    Add files via upload

---
+++ CSV_Combiner.py
@@ -0,0 +1,102 @@
+import csv
+import os
+import pathlib
+import sys
+
+
+class CSV_Combiner():
+    def __init__(self) -> None:
+        super().__init__()
+        # fixture_path = os.walk()
+
+    @classmethod
+    def file_validation(self, argv) -> bool:
+        # print(argv)
+        if len(argv) < 2:
+            print("Error: No files provided")
+            # print(
+            #     "Script Usage: python CSV_combiner.py ./file1.csv ./file2.csv  ./file3.csv  ")
+            return False
+
+        csv_files = argv[1:]
+        for filename in csv_files:
+            try:
+                # Check file extension
+                if pathlib.Path(filename).suffix != ".csv":
+                    raise TypeError(
+                        "Error: Invalid file type: \"{}\". Only pass .csv files".format(filename))
+
+                # check if files exist in the directory
+                if not os.path.exists(filename):
+                    raise FileNotFoundError(
+                        "Error : File not found: {}".format(filename))
+
+                # Check if file is not empty
+                if os.stat(filename).st_size == 0:
+                    raise OSError(
+                        "Warning: Passing empty file {}. File will be skipped".format(filename))
+
+            # Throw corresponding error
+            except FileNotFoundError as e:
+                print(e)
+                return False
+            except OSError as e:
+                print(e)
+            except TypeError as e:
+                print(e)
+                return False
+
+        return True
+
+    ''' 
+        Code Notes - 
+        1) Don't load the whole csv file to memory. 
+        3) Read each file line-by-line, make required modifations to the line. 
+        4) Write line in once its read in a stream fashion. 
+
+        Solution: Use similar approaches like fs.createreadstream  and fs.createwritestream in JS.
+    '''
+
+    def combiner(self, argv):
+        header_saved = False
+        if self.file_validation(argv):
+            files = argv[1:]
+
+            # open target file for writing. with clause will automatically close the file
+            with open("combined.csv", 'w', newline='') as target:
+                writer = csv.writer(
+                    target, delimiter=',', quoting=csv.QUOTE_NONE, escapechar='', quotechar='\n')
+
+                # iterate through each file
+                for filename in files:
+                    # print("Adding " + filename)  # debug print
+
+                    # open each file in read mode
+                    try:
+                        with open(filename, 'r') as current_file:
+
+                            # add header only once
+                            header = next(current_file).strip().split(",")
+                            header.append("filename")
+                            if not header_saved:
+                                print(header)
+                                writer.writerow(header)
+                                header_saved = True
+
+                            # Add rest to the target file
+                            for line in current_file:
+                                line_list = line.strip().split(',')
+                                file_location = os.path.basename(filename)
+                                line_list.append(file_location)
+                                print(",".join(line_list))  # debug print
+                                writer.writerow(line_list)
+
+                    except StopIteration:
+                        print("Blank file found and will be skipped")
+
+        return
+
+
+if __name__ == '__main__':
+    combiner = CSV_Combiner()
+    combiner.combiner(sys.argv)
---
+++ unitTesting.py
@@ -0,0 +1,82 @@
+import os
+import sys
+import csv
+import pathlib
+import unittest
+from CSV_Combiner import CSV_Combiner
+from io import StringIO
+import pandas as pd
+
+
+class test_csv_combiner(unittest.TestCase):
+
+    output_path = './test/combined.csv'
+    script_path = './CSV_Combiner.py'
+    blank_csv_path = './test/blank.csv'
+    test_fixtures_path = './test/fixtures'
+    random_extension_files_path = './test/random_files'
+
+    # instantiate combiner object
+    combiner_object = CSV_Combiner()
+    output_test = open(output_path, 'w')
+
+    @classmethod
+    def setUpClass(cls):
+        sys.stdout = cls.output_test
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.output_test.close()
+        if os.path.exists(cls.output_path):
+            os.remove(cls.output_path)
+
+    # For below functions,  followed -  https://docs.python.org/3/library/unittest.html
+    def setUp(self):
+        self.output = StringIO()
+        sys.stdout = self.output
+        self.output_test = open(self.output_path, 'w+')
+
+    def tearDown(self):
+        self.output_test.close()
+
+    # test 1
+    def test_no_file_pass(self):
+        argv = [self.script_path]
+        self.combiner_object.combiner(argv)
+        self.assertIn("Error: No files provided", self.output.getvalue())
+
+    # test 2
+    def test_empty_file(self):
+        argv = [self.script_path, self.blank_csv_path]
+        self.combiner_object.combiner(argv)
+
+        self.assertIn("Warning: Passing empty file ", self.output.getvalue())
+
+    # test 3
+    def test_file_extension(self):
+        argv = os.listdir(self.random_extension_files_path)
+        self.combiner_object.combiner(argv)
+        self.assertIn("Error: Invalid file type: ", self.output.getvalue())
+
+    # test 4
+    def test_file_not_exists(self):
+        argv = [self.script_path, "file_not_present.csv"]
+        self.combiner_object.combiner(argv)
+        self.assertIn("Error : File not found: ", self.output.getvalue())
+
+    # test 5
+    def test_filesource_column(self):
+        argv = [self.script_path] + os.listdir(self.test_fixtures_path)
+        self.combiner_object.combiner(argv)
+
+        self.output_test.write(self.output.getvalue())
+        self.output_test.close()
+
+        with open(self.output_path) as f:
+            header = next(f)
+
+        self.assertIn("filename", header)
+
+
+if __name__ == "__main__":
+    unittest.main()
